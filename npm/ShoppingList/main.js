6c21780b56b7f85f523f91a797892c4ef445117c
Kirandeep Kaur <741453@student.tor.loc>
2019-07-17 16:07:48 -0400

    abcd

--- npm/ShoppingList/main.js
+++ npm/ShoppingList/main.js
@@ -3,3 +3,16 @@ const url = require('url');
 const path = require('path');
 
 const { app, BrowserWindow } = electron;
+
+let mainWindow;
+// Listen for app to be ready
+
+app.on('ready', function() {
+    //Create new window
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(_dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+});

c46af38271f3d5d06a625c307cc1bfcf5b6adfdc
Kirandeep Kaur <741453@student.tor.loc>
2019-07-17 17:08:02 -0400

    5:07pm commit

--- npm/ShoppingList/main.js
+++ npm/ShoppingList/main.js
@@ -1,18 +1,16 @@
-const electron = require('electron');
-const url = require('url');
-const path = require('path');
-
-const { app, BrowserWindow } = electron;
-
+const electron  = require('electron');
+const url  = require('url');
+const path  = require('path');
+const {app, BrowserWindow} = electron;
 let mainWindow;
-// Listen for app to be ready
-
-app.on('ready', function() {
-    //Create new window
+// Listen for App to be Ready!
+app.on('ready', function(){
+    // create new Window
     mainWindow = new BrowserWindow({});
     mainWindow.loadURL(url.format({
-        pathname: path.join(_dirname, 'mainWindow.html'),
-        protocol: 'file:',
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol:'file:', 
         slashes: true
-    }))
+        }));
+        
 });
